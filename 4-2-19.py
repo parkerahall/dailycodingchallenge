75dd9c4b16f3a283b83cac5f94392d50dcd1dc6a
Parker Hall <parkerh@mit.edu>
2019-04-02 13:10:59 -0400

    added faster approach using assumption that string and target are in ASCII

--- 4-2-19.py
+++ 4-2-19.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
 
+# O(N) runtime, where N = |string|
 def string_to_dict(string):
     freq = defaultdict(int)
     for letter in string:
         freq[letter] += 1
     return freq
 
+# O(NM) runtime, where N = |string| and M = |word|
 def find_anagrams(string, word):
     word_len = len(word)
     word_dict = string_to_dict(word)
@@ -17,3 +19,29 @@ def find_anagrams(string, word):
 string = "abxaba"
 word = "ab"
 assert find_anagrams(string, word) == [0, 3, 4]
+
+# using the assumption the string is in ASCII, we can achieve a faster runtime
+
+# O(N) runtime, where N = max(|string|, |word|)
+def find_anagrams_better(string, word):
+    # 256 total possible characters in ASCII alphabet
+    word_len = len(word)
+    word_array = [0] * 256
+
+    for letter in word:
+        word_array[ord(letter)] += 1
+
+    string_array = [0] * 256
+    valid_starts = []
+    for i in range(len(string)):
+        if i >= word_len:
+            string_array[ord(string[i - word_len])] -= 1
+
+        string_array[ord(string[i])] += 1
+        if string_array == word_array:
+            valid_starts.append(i - word_len + 1)
+    return valid_starts
+
+string = "abxaba"
+word = "ab"
+assert find_anagrams_better(string, word) == [0, 3, 4]
