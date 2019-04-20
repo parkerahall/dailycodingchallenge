b9fd7dae41a82eb9b862e699454552f30ac07a82
Parker Hall <parkerh@mit.edu>
2019-04-19 21:02:29 -0400

    added __eq__ function for SinglyLinkedList

--- linked_list.py
+++ linked_list.py
@@ -3,6 +3,15 @@ class SinglyLinkedList:
         self.value = value
         self.next = nxt
 
+    def __eq__(self, other):
+        if not isinstance(other, SinglyLinkedList):
+            return False
+
+        if self.value != other.value:
+            return False
+        
+        return self.next == other.next
+
 class DoublyLinkedList:
     def __init__(self, value, prev=None, nxt=None):
         self.value = value
